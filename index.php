603ac7e307dfb55b6bdbb35ca487976ffe966a4a
pwauto939 <pwauto939@yandex.ru>
2017-10-02 17:40:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
 
 $output = json_decode(file_get_contents('php://input'),true);
-file_put_contents("logs.txt",$output);
+
+$text = $output['message']['text'];
+file_put_contents("logs.txt",$text);
