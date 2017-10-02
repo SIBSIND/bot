9843e1d5f99e203224c9c55c3e5857bb2eee54c1
pwauto939 <pwauto939@yandex.ru>
2017-10-02 19:08:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,8 +20,9 @@ function sendMessage($token, $id, $message)
 switch ($message)
 {
     case 'белово':
-        $message = 'ты выбрал белово, выбери гашиш';
+        $message = 'ты выбрал белово!';
         sendMessage($token, $id, $message.KeyboardMenu());
+        break;
 
     default:
         $message = 'Говно';
@@ -35,16 +36,13 @@ file_put_contents("logs.txt",$id);
 
 
 
-function KeyboardMenu()
-{
-    $buttons = [['hi'], ['privet']];
-    $keyboard = json_encode($keyboard =
-        [
-            'keyboard' => $buttons,
-            'resize_keyboard' => true,
-            'one_time_keyboard' => false,
-            'selective' => true
-        ]);
-    $reply_markup = '&reply_markup = ' . $keyboard . '';
+function KeyboardMenu(){
+    $buttons = [['hi'],['how are you']];
+    $keyboard =json_encode($keyboard = ['keyboard' => $buttons,
+                                        'resize_keyboard' => true,
+                                        'one_time_keyboard' => false,
+                                        'selective' => true]);
+    $reply_markup = '&reply_markup=' . $keyboard . '';
+
     return $reply_markup;
 }
