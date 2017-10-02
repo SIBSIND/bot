8b5b8d4852560763bd3f1fc78c025ade83cc6a5f
pwauto939 <pwauto939@yandex.ru>
2017-10-02 23:12:59 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
-mysqli_connect('mysql.hostinger.ru', 'u175867095_user', 'Ghjcnjq2');
-mysqli_select_db('u175867095_baza');
+mysql_connect('mysql.hostinger.ru', 'u175867095_user', 'Ghjcnjq2');
+mysql_select_db('u175867095_baza');
 ?>
