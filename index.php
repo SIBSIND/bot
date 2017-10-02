44774d649332908d7fcda73c3e66119dc591030d
pwauto939 <pwauto939@yandex.ru>
2017-10-02 17:55:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,8 @@ $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $text = $output['message']['text'];
 
-if($text == кто пидор?){
+if($text == "кто пидор?")
+{
 file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$id."&text=ты пидор");
 }
 
