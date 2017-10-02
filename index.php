b24a1f9be1cb0ec2ab53efb77c301ae4c608d391
pwauto939 <pwauto939@yandex.ru>
2017-10-02 17:51:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-
+$token = "332809777:AAHjqELf5LmeTgrqxWIp5BxtsTIi9upLsl4";
 $output = json_decode(file_get_contents('php://input'),true);
 
 $id = $output['message']['chat']['id'];
 
-file_get_contents("https://api.telegram.org/bot332809777:AAHjqELf5LmeTgrqxWIp5BxtsTIi9upLsl4/sendMessage?chat_id=".$id."&text=ты пидор");
+
+
+file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$id."&text=ты пидор");
 file_put_contents("logs.txt",$id);
