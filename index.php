de11d24895ff6ab772b6e0c1ad6ea17f996ad88f
pwauto939 <pwauto939@yandex.ru>
2017-10-02 19:42:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,31 +5,22 @@ $message = $output['message']['text'];
 $token = "332809777:AAHjqELf5LmeTgrqxWIp5BxtsTIi9upLsl4";
 
 
-
-function sendMessage($token, $id, $message)
+if($message == mb_strtolower('осел')
 {
-    file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=" . $message);
+    $message = "Пидорасы!";
+    sendMessage($token, $id, $message);
 }
-
-$if = "привет";
-
-KeyboardMenu($if, $message, $id, $token)
-
 
 
 
 file_put_contents("logs.txt",$id);
 
-
-function KeyboardMenu($if, $message, $id, $token)
+function sendMessage($token, $id, $message)
 {
-    if($message == mb_strtolower($if)
-        {
-        $message = "Пидорасы";
-        sendMessage($token, $id, $message);
-        }
+    file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=" . $message);
 }
-
+   
+   
 function KeyboardMenu(){
     $buttons = [['Паша'],['Ниджат']];
     $keyboard =json_encode($keyboard = ['keyboard' => $buttons,
