f8eb587d45005ea53beb09d23105d2d6222b8590
pwauto939 <pwauto939@yandex.ru>
2017-10-02 18:58:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,27 +5,8 @@ $message = $output['message']['text'];
 $token = "332809777:AAHjqELf5LmeTgrqxWIp5BxtsTIi9upLsl4";
 
 
-switch ($message)
-{
-    case 'белово':
-        $message = 'ты выбрал белово, выбери гашиш';
-        sendMessage($token, $id, $message);
-        
-            switch ($message) 
-            {
-                case 'гашиш':
-                $message = 'пизда рублю';
-                sendMessage($token, $id, $message);
-                break;
-            }
-        
 
-    default:
-        $message = 'Говно';
-        sendMessage($token, $id, $message);
-}
-
-//sendMessage($token, $id, $message);
+sendMessage($token, $id, $message);
 
 
 
@@ -34,4 +15,36 @@ function sendMessage($token, $id, $message)
     file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=" . $message);
 }
 
+
+
+switch ($message)
+{
+    case 'белово':
+        $message = 'ты выбрал белово, выбери гашиш';
+        sendMessage($token, $id, $message.KeyboardMenu());
+
+    default:
+        $message = 'Говно';
+        sendMessage($token, $id, $message);
+}
+
+
+
+
 file_put_contents("logs.txt",$id);
+
+
+
+function KeyboardMenu()
+{
+    $buttons = [['hi'], ['privet']];
+    $keyboard = json_encode($keyboard =
+        [
+            'keyboard' => $buttons,
+            'resize_keyboard' => true,
+            'one_time_keyboard' => false,
+            'selective' => true
+        ]);
+    $reply_markup = '&reply_markup = ' . $keyboard . '';
+    return $reply_markup;
+}
