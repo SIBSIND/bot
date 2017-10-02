af9280cea52fc3c8e9b90a6af19b5179bf9b5bcf
pwauto939 <pwauto939@yandex.ru>
2017-10-02 21:50:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,12 @@ $id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
 $token = "332809777:AAHjqELf5LmeTgrqxWIp5BxtsTIi9upLsl4";
 
-
+include "connect.php";
 
 function sendMessage($token, $id, $message)
 {
     file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=" . $message);
 }
-
-
 
 switch ($message)
 {
