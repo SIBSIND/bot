af7ab6496eb6e1bb0637270cbaf938bc4f40eb6e
pwauto939 <pwauto939@yandex.ru>
2017-10-04 15:22:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -19,11 +19,15 @@ if($message == "/start")
     if (!$row) 
     {
     mysqli_query($connect, "INSERT INTO `users` (`chatid`) VALUES ($id)");
+    $message = "Вы зарегистрировались! Ваш chatid: $id";
+    sendMessage($token, $id, $message);
     }
     $message = "Привет, меня зовут Бот Антон! ";
     sendMessage($token, $id, $message.KeyboardMenu());
     $message = 'Попав сюда, ты встретил самого выгодного телеграм бота! Выбери, чем ты хочешь заняться?';
     sendMessage($token, $id, $message.KeyboardMenu());
+    $message = "Вы зарегистрированы! Ваш chatid: $id";
+    sendMessage($token, $id, $message);
 }
 
 
