1a2c632e0b4fa81c7222edf53c2fe1f1e6705542
pwauto939 <pwauto939@yandex.ru>
2017-10-05 18:18:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,17 +5,22 @@ if(!$connect) exit();
 $output = json_decode(file_get_contents('php://input'),true);
 $id = $output['message']['chat']['id'];
 $message = $output['message']['text'];
-$token = "332809777:AAHjqELf5LmeTgrqxWIp5BxtsTIi9upLsl4";
+$token = "353692369:AAG8oKq8id2URz9Xzr8RFGlYOAWFA30A0LI";
 
 function sendMessage($token, $id, $message)
 {
     file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=" . $message);
 }
 
+if( $message == "/start" or $message == "В главное меню")
+{
+	$query = mysqli_query($connect, "SELECT * FROM `settings` WHERE `botid` = 1");
+	$fetch = mysqli_fetch_assoc($query);
+    $welcome = $fetch['welcome'];
     $but1 = "Пидорасы";
     $but2 = "Хуесосы";
-    sendMessage($token, $id, $message.KeyboardMenu($but1,$but2));
-
+    sendMessage($token, $id, $msg.KeyboardMenu($but1,$but2));
+}
 
 
 file_put_contents("logs.txt",$connection);
