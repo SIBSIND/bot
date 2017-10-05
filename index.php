8c13e91e3e36afedae16fd14432899de07c6d546
pwauto939 <pwauto939@yandex.ru>
2017-10-05 18:19:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -16,9 +16,10 @@ if( $message == "/start" or $message == "В главное меню")
 {
 	$query = mysqli_query($connect, "SELECT * FROM `settings` WHERE `botid` = 1");
 	$fetch = mysqli_fetch_assoc($query);
-    $welcome = $fetch['welcome'];
-    $but1 = "Пидорасы";
-    $but2 = "Хуесосы";
+	$welcome = $fetch['welcome'];
+	$msg = $welcome . "Отныне мир будет моим!";
+	$but1 = "Пидорасы";
+	$but2 = "Хуесосы";
     sendMessage($token, $id, $msg.KeyboardMenu($but1,$but2));
 }
 
