6f5b49fa4692c23393878568a4fe9cab23351b8b
pwauto939 <pwauto939@yandex.ru>
2017-10-05 19:09:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -39,9 +39,9 @@ file_put_contents("logs.txt",$connection);
 
 
 function KeyboardMenu($but1,$but2,$but3,$but4,$but5,$but6,$but7,$but8,$but9,$but10,$but11,$but12,$but13){
-	$buttons = [[$but1],[$but2],[$but3],[$but4],[$but5],[$but6],[$but7],[$but8],[$but9],[$but10]];
+	$buttons = [[$but1],[$but2],[$but3],[$but4],[$but5],[$but6],[$but7],[$but8],[$but9],[$but10],[$but11],[$but12],[$but13]]];
 	$buttons1 = [[$but11],[$but12],[$but13]];
-	$keyboard = json_encode($keyboard = ['keyboard' => $buttons1, 'keyboard' => $buttons1,
+	$keyboard = json_encode($keyboard = ['keyboard' => $buttons, 'keyboard' => $buttons1,
         'resize_keyboard' => true,
         'one_time_keyboard' => false,
         'selective' => true]);
