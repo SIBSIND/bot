dc3add5824b4f6ca334f1353d5e0b30608c7799b
pwauto939 <pwauto939@yandex.ru>
2017-10-06 23:03:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -40,7 +40,8 @@ function sendMessage($token, $id, $message)
 	$cat4 = $fetchc['cat4'];
 	$cat5 = $fetchc['cat5'];
 
-if( $message == "/start" or $message == "В главное меню"){
+if( $message == "/start" or $message == "В главное меню")
+{
 	$queryc = mysqli_query($connect, "SELECT * FROM `users` WHERE `chatid` = $id");
 	$row = mysqli_num_rows($queryc);
 	if(!$row)
@@ -53,6 +54,7 @@ if( $message == "/start" or $message == "В главное меню"){
 		$msg = $welcome . urlencode("\n\nОтзывы покупателей (нажмите 👉 /otzivi)\nОставить отзыв (нажмите 👉 /otziv)\n\nДля покупки нажмите на свой город внизу:");
 		sendMessage($token, $id, $msg.KeyboardMenu($but1,$but2,$but3,$but4,$but5,$but6,$but7,$but8,$but9,$but10,$but11,$but12,$but13));	
 	}
+}
 
 if($message == $but1)
 {
