52ce3e421d9ad0a90a86789aaa4f362d84068960
pwauto939 <pwauto939@yandex.ru>
2017-10-06 22:43:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -640,6 +640,9 @@ else if($message == $but10)
 	}
 }
 
+$queryc = mysqli_query($connect, "SELECT * FROM `cat` WHERE `botid` = $botid");
+$fetchc = mysqli_fetch_assoc($queryc);
+$cat1 = $fetchc['cat1'];
 if($check == 228 and $message == $cat1)
 {
 	$msg = "Вы выбрали Яблоки";
