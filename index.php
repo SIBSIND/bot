8ccb795046a2c635dd686063fc388060c37c0da2
pwauto939 <pwauto939@yandex.ru>
2017-10-09 21:13:58 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2193,6 +2193,28 @@ else if($cat == 5)
 
 if($message == $tovname1 and $categ > 0)
 {
+	$query = mysqli_query($connect, "SELECT * FROM `fas` WHERE `botid` = '$botid' and `fasid` = 1");
+	$assoc = mysqli_fetch_assoc($query);
+	$fas1 = $assoc['fas'];
+	$query = mysqli_query($connect, "SELECT * FROM `tovar` WHERE `botid` = '$botid' and `fas` = $fas1");
+	$row = mysqli_num_rows($query);
+	if($row){$fas1 = $assoc['fas'];}else {$fas1 = "";}
+	
+	$query = mysqli_query($connect, "SELECT * FROM `fas` WHERE `botid` = '$botid' and `fasid` = 2");
+	$assoc = mysqli_fetch_assoc($query);
+	$fas2 = $assoc['fas'];
+	$query = mysqli_query($connect, "SELECT * FROM `tovar` WHERE `botid` = '$botid' and `fas` = $fas2");
+	$row = mysqli_num_rows($query);
+	if($row){$fas2 = $assoc['fas'];}else {$fas2 = "";}
+	
+	
+	$query = mysqli_query($connect, "SELECT * FROM `fas` WHERE `botid` = '$botid' and `fasid` = 3");
+	$assoc = mysqli_fetch_assoc($query);
+	$fas3 = $assoc['fas'];
+	$query = mysqli_query($connect, "SELECT * FROM `tovar` WHERE `botid` = '$botid' and `fas` = $fas3");
+	$row = mysqli_num_rows($query);
+	if($row){$fas3 = $assoc['fas'];}else {$fas3 = "";}
+	
 	mysqli_query($connect, "UPDATE `users` SET `tovid` = '1' WHERE `users`.`botid` = $botid");
 	$msg = "Вы выбрали "  . $tovname1 . urlencode("\n\n▪▪▪▪▪▪▪▪▪▪\nГОРОД: ") . $city . urlencode("\nКАТЕГОРИЯ: ") . $cat . urlencode("\nТОВАР: ") . $tovname1 . urlencode("\n▪▪▪▪▪▪▪▪▪▪\nВыберите фасовку:");
 	sendMessage($token, $id, $msg);
