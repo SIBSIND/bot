14d4e1f3e82e3e19dee8533a5861305a2ae27d80
pwauto939 <pwauto939@yandex.ru>
2017-10-09 20:35:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2188,20 +2188,12 @@ else if($cat == 5)
 }
 
 
-$query = mysqli_query($connect, "SELECT * FROM `fas` WHERE `fasid` = 1");
-$fas1 = $query['fas'];
-$query = mysqli_query($connect, "SELECT * FROM `fas` WHERE `fasid` = 2");
-$fas2 = $query['fas'];
-$query = mysqli_query($connect, "SELECT * FROM `fas` WHERE `fasid` = 3");
-$fas3 = $query['fas'];
-
-
 if($message == $tovname1 and $categ > 0)
 {
 
 	mysqli_query($connect, "UPDATE `users` SET `tovid` = '1' WHERE `users`.`botid` = $botid");
 	$msg = "Вы выбрали "  . $tovname1 . urlencode("\n\n▪▪▪▪▪▪▪▪▪▪\nГОРОД: ") . $city . urlencode("\nКАТЕГОРИЯ: ") . $cat . urlencode("\nТОВАР: ") . $tovname1 . urlencode("\n▪▪▪▪▪▪▪▪▪▪\nВыберите фасовку:");
-	sendMessage($token, $id, $msg.KeyboardMenuFas($fas1, $fas2, $fas3, $but11, $but12, $but13));
+	sendMessage($token, $id, $msg);
 }
 if($message == $tovname2 and $categ > 0)
 {
