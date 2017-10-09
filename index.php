1436c34211506ebec374943f738df332dd705a80
pwauto939 <pwauto939@yandex.ru>
2017-10-09 20:44:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2200,19 +2200,19 @@ if($message == $tovname1 and $categ > 0)
 	$query = mysqli_query($connect, "SELECT * FROM `tovar` WHERE `fas` = '$fas1' and `name` = '$tovname1'");
 	$row = mysqli_num_rows($query);
 	$fetch = mysqli_fetch_assoc($query);
-	if($row){
+	if($row >=1){
 		$fas1 = $row['fas'];
 	}
 	$query = mysqli_query($connect, "SELECT * FROM `tovar` WHERE `fas` = '$fas2' and `name` = '$tovname1'");
 	$row = mysqli_num_rows($query);
 	$fetch = mysqli_fetch_assoc($query);
-	if($row){
+	if($row >=1){
 		$fas2 = $row['fas'];
 	}
 	$query = mysqli_query($connect, "SELECT * FROM `tovar` WHERE `fas` = '$fas3' and `name` = '$tovname1'");
 	$row = mysqli_num_rows($query);
 	$fetch = mysqli_fetch_assoc($query);
-	if($row){
+	if($row >=1){
 		$fas3 = $row['fas'];
 	}
 	mysqli_query($connect, "UPDATE `users` SET `tovid` = '1' WHERE `users`.`botid` = $botid");
