df91382a93e74e50b31bfa0f581be8ce638e2947
pwauto939 <pwauto939@yandex.ru>
2017-10-11 19:40:33 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2308,15 +2308,14 @@ mysqli_query($connect, "UPDATE `users` SET `fas` = '5' WHERE `users`.`chatid` = 
 file_put_contents("logs.txt",$message);
 
 
-function KeyboardMenu($but1,
-		      $but2,$but3,$but4,$but5,$but6,$but7,$but8,$but9,$but10,$but11,$but12,$but13){
+function KeyboardMenu($but1,$but2,$but3,$but4,$but5,$but6,$but7,$but8,$but9,$but10,$but11,$but12,$but13){
 	$buttons = [
 		   ['$but1', '$but2'],
 		   ['$but3', '$but4'],
 		   ['$but5', '$but6'],
 		   ['$but7','$but8'],
 		   ['$but9','$but10'],
-	     ['$but11' , '$but12', '$but13]
+	     ['$but11' , '$but12', '$but13']
 		   ];
 	$keyboard = json_encode($keyboard = ['keyboard' => $buttons,
         'resize_keyboard' => true,
