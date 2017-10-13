f51042fc7166aba4e50e51bf32281e02a358d025
pwauto939 <pwauto939@yandex.ru>
2017-10-14 01:53:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2309,7 +2309,7 @@ else if($tovid == 5){$tov = $tovname5;}
 $msg = "Вы выбрали "  . $message . urlencode("\n\n▪▪▪▪▪▪▪▪▪▪\nГОРОД: ") . $city . urlencode("\nКАТЕГОРИЯ: ") . $cat . urlencode("\nТОВАР: ") . $tov . urlencode("\nФАСОВКА: ") . $message . urlencode("\n▪▪▪▪▪▪▪▪▪▪\nВыберите район:");
 	
 	
-$query = mysqli_query($connect, "SELECT * `reg` WHERE `botid` = 1 and `regid` = 1");
+$query = mysqli_query($connect, "SELECT * FROM `reg` WHERE `botid` = $botid and `regid` = 1");
 $row = mysqli_fetch_assoc($query);
 $regname1 = $row['reg'];
 
