95d31e4042ddaea1c522068d1278895c0d53d3dd
pwauto939 <pwauto939@yandex.ru>
2017-10-14 00:51:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2295,6 +2295,15 @@ $mes = (float)$message;
 
 if($mes == $fasname1 and $tovid > 0 )
 {
+$query = mysqli_query($connect, "SELECT * FROM `users` WHERE `chatid` = $id");
+$row = mysqli_fetch_assoc($query);
+$tovid = $row['tovid'];
+if($tovid == 1)($tov = $tovname1)
+else if($tovid == 2){$tov = $tovname2}
+else if($tovid == 3){$tov = $tovname3}
+else if($tovid == 4){$tov = $tovname4}
+else if($tovid == 5){$tov = $tovname5}
+	
 $msg = "Вы выбрали "  . $message . urlencode("\n\n▪▪▪▪▪▪▪▪▪▪\nГОРОД: ") . $city . urlencode("\nКАТЕГОРИЯ: ") . $cat . urlencode("\nТОВАР: ") . $tovname1 . urlencode("\nФАСОВКА: ") . $message . urlencode("\n▪▪▪▪▪▪▪▪▪▪\nВыберите район:");
 mysqli_query($connect, "UPDATE `users` SET `fas` = '1' WHERE `users`.`chatid` = $id");
 sendMessage($token, $id, $msg);
