85770aeb929536f9f8f7c39b4828998f5448f427
pwauto939 <pwauto939@yandex.ru>
2017-10-18 23:18:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2727,10 +2727,10 @@ if($message == "QIWI")
 
 if($message == "Проверить оплату" and $payid == 1)
 {
-	$query = mysqli_query($connect, "SELECT * FROM `cat` WHERE `botid` = $botid");
+	$query = mysqli_query($connect, "SELECT * FROM `user` WHERE `chatid` = $id");
 	$row = mysqli_fetch_assoc($query);
 	$price = $row['price'];
-    $query = file_get_contents('https://qiwigate.ru/api?key=ZKCYWVA1TD67N34PQHAIO8DPXL5LSE&method=qiwi.get.history&start=25.09.2017&finish=27.09.2019');
+	$query = file_get_contents('https://qiwigate.ru/api?key=ZKCYWVA1TD67N34PQHAIO8DPXL5LSE&method=qiwi.get.history&start=25.09.2017&finish=27.09.2019');
 	$json = json_decode($query, true);
 	$json = $json['history'];
 
