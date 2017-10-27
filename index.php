f505b96870fe2480f1c381377afa57bb54e877f0
pwauto939 <pwauto939@yandex.ru>
2017-10-27 19:07:40 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,124 +3,23 @@ $connect = mysqli_connect('a0160954.xsph.ru:3306','a0160954_bazis','Ghjcnjq2','a
 if(!$connect) exit();
 $botid = 1;
 $nomer1 = "79654405539";
-$output = json_decode(file_get_contents('php://input'),true);
-$id = $output['message']['chat']['id'];
-$message = $output['message']['text'];
-$token = "353692369:AAG8oKq8id2URz9Xzr8RFGlYOAWFA30A0LI";
 
-$query = mysqli_query($connect, "SELECT * FROM `users`");
-$query = mysqli_query($connect, "SELECT * FROM `users`");
-$query = mysqli_query($connect, "SELECT * FROM `users`");
-$query = mysqli_query($connect, "SELECT * FROM `users`");
-$query = mysqli_query($connect, "SELECT * FROM `users`");
-$query = mysqli_query($connect, "SELECT * FROM `users`");
-$query = mysqli_query($connect, "SELECT * FROM `users`");
 
-function sendMessage($token, $id, $message)
+global $token = "6d0600a19121b7a5ff6ed03ab7016c93ef53992ec2c1533999c3046c1859bf604478094ba5d6da3733ee3";
+$id = "400547697";
+
+send($id);
+
+function send($id)
 {
-    file_get_contents("https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $id . "&text=" . $message);
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl, CURLOPT_URL, 'https://api.vk.com/method/messages.send?user_id=' . $id . '&message=ТУТ%20РУССКИЕ%20БУВЫ%20И%20ПРОБЕЛЫ?&access_token=' . $token);
+    $response = curl_exec($curl);
+    curl_close($curl);
+    $response = json_decode($response, true);
+    return $response;
 }
 
-if( $message == "/start" or $message == "В главное меню")
-{
-    $query = mysqli_query($connect, "SELECT * FROM `users`");
-    $query = mysqli_query($connect, "SELECT * FROM `users`");
-    $query = mysqli_query($connect, "SELECT * FROM `users`");
-    $query = mysqli_query($connect, "SELECT * FROM `users`");
-    $query = mysqli_query($connect, "SELECT * FROM `users`");
-    $query = mysqli_query($connect, "SELECT * FROM `users`");
-    $query = mysqli_query($connect, "SELECT * FROM `users`");
-    $query = mysqli_query($connect, "SELECT * FROM `users`");
-    $row = mysqli_num_rows($query);
-    if($row) {$users = $row;}
-    $msg = "Привет " . $row . " человек";
-    sendMessage($token, $id, $msg);         
-}
-
-
-file_put_contents("logs.txt",$message);
-
-
-function KeyboardMenu($but1,$but2,$but3,$but4,$but5,$but6,$but7,$but8,$but9,$but10,$but11,$but12,$but13){
-    $buttons = [[$but1, $but2],[$but3, $but4],[$but5, $but6],[$but7,$but8],[$but9,$but10],[$but11 , $but12, $but13]];
-    $keyboard = json_encode($keyboard = ['keyboard' => $buttons,
-        'resize_keyboard' => true,
-        'one_time_keyboard' => false,
-        'selective' => true]);
-    $reply_markup = '&reply_markup=' . $keyboard . '';
-
-    return $reply_markup;
-}
-
-function KeyboardMenuWall($wall1, $wall2, $but11, $but12, $but13){
-    $buttons = [[$wall1,$wall2],[$but11,$but12,$but13]];
-    $keyboard = json_encode($keyboard = ['keyboard' => $buttons,
-        'resize_keyboard' => true,
-        'one_time_keyboard' => false,
-        'selective' => true]);
-    $reply_markup = '&reply_markup=' . $keyboard . '';
-
-    return $reply_markup;
-}
-
-function KeyboardMenuMenu($check, $but11, $but12, $but13){
-    $buttons = [[$check],[$but11,$but12,$but13]];
-    $keyboard = json_encode($keyboard = ['keyboard' => $buttons,
-        'resize_keyboard' => true,
-        'one_time_keyboard' => false,
-        'selective' => true]);
-    $reply_markup = '&reply_markup=' . $keyboard . '';
-
-    return $reply_markup;
-}
-
-function KeyboardMenuCat($cat1, $cat2, $cat3, $cat4, $cat5, $but11, $but12, $but13){
-    $buttons = [[$cat1,$cat2],[$cat3,$cat4],[$cat5],[$but11,$but12,$but13]];
-    $keyboard = json_encode($keyboard = ['keyboard' => $buttons,
-        'resize_keyboard' => true,
-        'one_time_keyboard' => false,
-        'selective' => true]);
-    $reply_markup = '&reply_markup=' . $keyboard . '';
-
-    return $reply_markup;
-}
-
-function KeyboardMenuReg($reg1, $reg2, $reg3, $reg4, $reg5, $but11, $but12, $but13){
-    $buttons = [[$reg1,$reg2],[$reg3,$reg4],[$reg5],[$but11,$but12,$but13]];
-    $keyboard = json_encode($keyboard = ['keyboard' => $buttons,
-        'resize_keyboard' => true,
-        'one_time_keyboard' => false,
-        'selective' => true]);
-    $reply_markup = '&reply_markup=' . $keyboard . '';
-
-    return $reply_markup;
-}
-
-function KeyboardMenuTov($tov1, $tov2, $tov3, $tov4, $tov5, $but11, $but12, $but13){
-    $buttons = [[$tov1,$tov2],[$tov3,$tov4],[$tov5],[$but11,$but12,$but13]];
-    $keyboard = json_encode($keyboard = ['keyboard' => $buttons,
-        'resize_keyboard' => true,
-        'one_time_keyboard' => false,
-        'selective' => true]);
-    $reply_markup = '&reply_markup=' . $keyboard . '';
-
-    return $reply_markup;
-}
-
-function KeyboardMenuFas($fas1, $fas2, $fas3, $but11, $but12, $but13){
-    $buttons = [[$fas1],[$fas2],[$fas3],[$but11,$but12,$but13]];
-    $keyboard = json_encode($keyboard = ['keyboard' => $buttons,
-        'resize_keyboard' => true,
-        'one_time_keyboard' => false,
-        'selective' => true]);
-    $reply_markup = '&reply_markup=' . $keyboard . '';
-
-    return $reply_markup;
-}
-
- function ReplyKeyboardRemove(){
-        $removeKeyboard = json_encode($removeKeyboard = ['remove_keyboard' => true]);
-        $reply_markup = '&reply_markup=' . $removeKeyboard . '';
-        return $reply_markup;
-    }
 ?>
